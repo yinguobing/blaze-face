8df5cde42a823942e71ed680cad8cfdb97e34737
yinguobing <yinguobing@gmail.com>
2020-11-05 15:04:58 +0800

    add module for preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,19 @@
+"""Preprocessing functions for BlazeFace"""
+
+import numpy as np
+
+
+def normalize(image):
+    """Normalize the input image.
+
+    Args:
+        inputs: a RGB image as numpy array.
+
+    Returns:
+        a normalized image.
+    """
+    img_mean = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+    img_std = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+
+    # Normalization
+    return ((inputs / 255.0) - img_mean)/img_std
