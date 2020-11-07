4300199bd27f91c9af1a37c999dc86e895d4e81d
yinguobing <yinguobing@gmail.com>
2020-11-07 18:06:42 +0800

    fix shapes in the loss function

--- losses.py
+++ losses.py
@@ -9,53 +9,36 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-class RetinaNetBoxLoss(keras.losses.Loss):
-    """Implements Smooth L1 loss"""
-
-    def __init__(self, delta):
-        super(RetinaNetBoxLoss, self).__init__(reduction="none",
-                                               name="RetinaNetBoxLoss")
-        self._delta = delta
-
-    def call(self, y_true, y_pred):
-        difference = y_true - y_pred
-        absolute_difference = tf.abs(difference)
-        squared_difference = difference ** 2
-        loss = tf.where(tf.less(absolute_difference, self._delta),
-                        0.5 * squared_difference,
-                        absolute_difference - 0.5,)
-        return tf.reduce_sum(loss, axis=-1)
+def boxes_loss(delta, y_true, y_pred):
+    difference = y_true - y_pred
+    absolute_difference = tf.abs(difference)
+    squared_difference = difference ** 2
+    loss = tf.where(tf.less(absolute_difference, delta),
+                    0.5 * squared_difference,
+                    absolute_difference - 0.5,)
+    return tf.reduce_sum(loss, axis=-1)
 
 
-class RetinaNetClassificationLoss(keras.losses.Loss):
+def cls_loss(alpha, gamma, y_true, y_pred):
     """Implements Focal loss"""
-
-    def __init__(self, alpha, gamma):
-        super(RetinaNetClassificationLoss, self).__init__(
-            reduction="none", name="RetinaNetClassificationLoss")
-        self._alpha = alpha
-        self._gamma = gamma
-
-    def call(self, y_true, y_pred):
-        cross_entropy = keras.losses.BinaryCrossentropy()(labels=y_true,
-                                                          logits=y_pred)
-        probs = tf.nn.sigmoid(y_pred)
-        alpha = tf.where(tf.equal(y_true, 1.0),
-                         self._alpha, (1.0 - self._alpha))
-        pt = tf.where(tf.equal(y_true, 1.0), probs, 1 - probs)
-        loss = alpha * tf.pow(1.0 - pt, self._gamma) * cross_entropy
-        return tf.reduce_sum(loss, axis=-1)
+    cross_entropy = keras.losses.BinaryCrossentropy()(y_true, y_pred)
+    probs = tf.nn.sigmoid(y_pred)
+    alpha = tf.where(tf.equal(y_true, 1.0),
+                     alpha, (1.0 - alpha))
+    pt = tf.where(tf.equal(y_true, 1.0), probs, 1 - probs)
+    loss = alpha * tf.pow(1.0 - pt, gamma) * cross_entropy
+    return tf.reduce_sum(loss, axis=-1)
 
 
 class RetinaNetLoss(keras.losses.Loss):
     """Wrapper to combine both the losses"""
 
-    def __init__(self, num_classes=1, alpha=0.25, gamma=2.0, delta=1.0):
+    def __init__(self, alpha=0.25, gamma=2.0, delta=1.0):
         super(RetinaNetLoss, self).__init__(
             reduction="auto", name="RetinaNetLoss")
-        self._clf_loss = RetinaNetClassificationLoss(alpha, gamma)
-        self._box_loss = RetinaNetBoxLoss(delta)
-        self._num_classes = num_classes
+        self._alpha = alpha
+        self._gamma = gamma
+        self._delta = delta
 
     def call(self, y_true, y_pred):
         y_pred = tf.cast(y_pred, dtype=tf.float32)
@@ -64,13 +47,19 @@ class RetinaNetLoss(keras.losses.Loss):
                                 dtype=tf.float32)
         ignore_mask = tf.cast(tf.equal(y_true[:, :, 4], -2.0),
                               dtype=tf.float32)
+        normalizer = tf.reduce_sum(positive_mask, axis=-1)
 
         # Loss for classifications.
         cls_labels = y_true[:, :, 4]
         cls_predictions = y_pred[:, :, 4]
 
-        clf_loss = self._clf_loss(cls_labels, cls_predictions)
-        clf_loss = tf.where(tf.equal(ignore_mask, 1.0), 0.0, clf_loss)
+        clf_loss = cls_loss(self._alpha, self._gamma,
+                            cls_labels, cls_predictions)
+
+        clf_loss = tf.where(tf.equal(ignore_mask, 1.0),
+                            tf.zeros_like(ignore_mask), 
+                            tf.expand_dims(clf_loss, axis=-1))
+
         clf_loss = tf.math.divide_no_nan(
             tf.reduce_sum(clf_loss, axis=-1), normalizer)
 
@@ -78,13 +67,12 @@ class RetinaNetLoss(keras.losses.Loss):
         box_labels = y_true[:, :, :4]
         box_predictions = y_pred[:, :, :4]
 
-        box_loss = self._box_loss(box_labels, box_predictions)
-        box_loss = tf.where(tf.equal(positive_mask, 1.0), box_loss, 0.0)
-        normalizer = tf.reduce_sum(positive_mask, axis=-1)
-        box_loss = tf.math.divide_no_nan(
-            tf.reduce_sum(box_loss, axis=-1), normalizer)
+        regr_loss = boxes_loss(self._delta, box_labels, box_predictions)
+        regr_loss = tf.where(tf.equal(positive_mask, 1.0), regr_loss, 0.0)
+        regr_loss = tf.math.divide_no_nan(
+            tf.reduce_sum(regr_loss, axis=-1), normalizer)
 
         # Total loss.
-        loss = clf_loss + box_loss
+        loss = clf_loss + regr_loss
 
         return loss
